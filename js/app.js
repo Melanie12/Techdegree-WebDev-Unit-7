cec034ba09e99e6b16bcde0eacc8290fc136a39b
Melanie Almeida <almeida.melanie@gmail.com>
2019-11-02 12:27:30 +0100

    charts

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ alert.addEventListener('click', e => {
 });
 
 /*Chart JS*/
+let trafficCanvas=document.getElementById("traffic-chart");
 let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
   "4-10", "11-17", "18-24", "25-31"],
@@ -43,3 +44,88 @@ let trafficOptions = {
     display: false
   }
 };
+
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+});
+
+/*Bar Chart*/
+const dailyCanvas = document.getElementById("daily-chart");
+// data for daily traffic bar chart
+const dailyData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+}]
+};
+const dailyOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+  beginAtZero:true
+  }
+  }]
+},
+legend : {
+  display: false
+}
+}
+let dailyChart = new Chart(dailyCanvas, {
+type: 'bar',
+data: dailyData,
+options: dailyOptions
+});
+
+//Doughnut Chart
+const mobileCanvas = document.getElementById("mobile-chart");
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ]
+  }]
+};
+
+const mobileOptions = {
+legend: {
+  position: 'right',
+  labels: {
+    boxWidth: 20,
+    fontStyle: 'bold'
+    }
+  }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
+});
+
+//Messaging section
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+send.addEventListener('click', () => {
+  // ensure user and message fields are filled out
+  if (user.value === "" && message.value === "") {
+  alert("Please fill out user and message fields before sending");
+  } else if (user.value === "" ) {
+  alert("Please fill out user field before sending");
+  } else if (message.value === "" ) {
+  alert("Please fill out message field before sending");
+  } else {
+  alert(`Message successfully sent to: ${user.value}`);
+}
+});
